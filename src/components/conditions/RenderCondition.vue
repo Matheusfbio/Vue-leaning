334fd1aefbeba74eed632f272d4892033309ab3e
Matheus Fábio <matheusfabiors@gmail.com>
2024-10-31 00:53:10 -0300

    Vue learning

---
+++ src/components/conditions/RenderCondition.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ShowDataUsers from '../user/ShowDataUsers.vue'
+const show = true
+</script>
+<style>
+.showColor {
+  color: red;
+}
+</style>
+
+<template>
+  <div v-if="show" class="showColor">
+    <h1>account</h1>
+    <ShowDataUsers />
+  </div>
+</template>
---
+++ src/components/user/ShowDataUsers.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const name = 'Matheus fabio'
+</script>
+
+<template>
+  <main>
+    <h1>{{ name }}</h1>
+  </main>
+</template>
